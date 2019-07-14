747495d7a82f750b3f89bb3aa6bce4c74d7ba5d1
IronPig <johnstonethepepsi@gmail.com>
2019-07-14 18:20:59 +0200

    Use vim for opening files with their shortcut/alias instead of that complicated expression

--- .scripts/shortcut-generator.sh
+++ .scripts/shortcut-generator.sh
@@ -4,7 +4,7 @@ while read -r alias path; do
 	eval path=$path
     [ -d "$path" ] \
     && command=cd \
-    || command="${VISUAL:-${EDITOR-:vi}}"
+    || command="vim"
 
     printf "alias %s='%s %s'\n" "$alias" "$command" "$path"
 done < ~/.config/shortcuts > ~/.config/aliasrc
